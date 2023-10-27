6ae013f109215cc1075394c01afd90744c65a0a3
Ari-mu-l <xiaohe_shen@brown.edu>
2023-10-27 22:39:09 +0200

    updated makeFileLists and directories for a more general application

--- GenXSecAnalyzer/src/makeFileLists.py
+++ GenXSecAnalyzer/src/makeFileLists.py
@@ -1,27 +1,36 @@
 # cd ..
-# python src/makeFileLists.py Drell-Yan
+# python src/makeFileLists.py StandardModelPhysics Drell-Yan
 
 import os, sys
-import StandardModelPhysics
 
-samples = {"Drell-Yan"  :StandardModelPhysics.sampleInfo["Drell-Yan"],
-           "ElectroWeak":StandardModelPhysics.sampleInfo["ElectroWeak"],
-           "MinimumBias":StandardModelPhysics.sampleInfo["MinimumBias"],
-           "QCD"        :StandardModelPhysics.sampleInfo["QCD"],
-           "TopPhysics" :StandardModelPhysics.sampleInfo["TopPhysics"],
-       }
+section = sys.argv[1]
+process = sys.argv[2]
 
-process = sys.argv[1]
+if (section == "StandardModelPhysics"):
+    import StandardModelPhysics
+    samples = {"Drell-Yan"  :StandardModelPhysics.sampleInfo["Drell-Yan"],
+               "ElectroWeak":StandardModelPhysics.sampleInfo["ElectroWeak"],
+               "MinimumBias":StandardModelPhysics.sampleInfo["MinimumBias"],
+               "QCD"        :StandardModelPhysics.sampleInfo["QCD"],
+               "TopPhysics" :StandardModelPhysics.sampleInfo["TopPhysics"],
+           }
+
+if (section == "HiggsPhysics"):
+    import HiggsPhysics
+    samples = {#"BeyondStandardModel": HiggsPhysics.sampleInfo["BeyondStandardModel"],
+               "StandardModel": HiggsPhysics.sampleInfo["StandardModel"],
+}
+
 count = 0
 
 for recid in samples[process]:
     try:
-        cmd = "cernopendata-client get-file-locations --recid {} --protocol xrootd > fileLists/{}/recid_{}.txt".format(recid, process, recid)
+        cmd = "cernopendata-client get-file-locations --recid {} --protocol xrootd > fileLists/{}/{}/recid_{}.txt".format(recid, section, process, recid)
         os.system(cmd)
         count+=1
-        #if (count%10==0):
-        print("Created {} lists.".format(count))
+        if (count%10==0):
+            print("Created {} lists.".format(count))
     except:
         print("Failed to create recid_{}.txt".format(recid))
 
-print("{} {} filelists created.".format(count, sys.argv[1]))
+print("{} {} filelists created.".format(count, process))
