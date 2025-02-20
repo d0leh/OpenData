e96fd446a3a11fb280819e65b3b4116e1cc9d16f
Mohammad Doleh <d0leh@users.noreply.github.com>
2025-02-21 01:15:12 +0300

    File renaiming

---
+++ GenXSecAnalyzer/src/runRecursive_DAS2017.py
@@ -0,0 +1,25 @@
+# cd ..
+# python src/runRecursive_DAS.py 2017 StandardModelPhysics Drell-Yan
+
+import os, sys
+#import StandardModelPhysics2016
+# import HiggsPhysics2016
+
+year    = sys.argv[1]
+section = sys.argv[2]
+process = sys.argv[3]
+skipexisting = False
+
+count = 0
+#directory = "fileLists/{}/{}/{}/".format(year, section, process)
+files = os.listdir("fileLists/{}/{}/{}/".format(year, section, process))
+
+for txtfile in files:
+    txtfile = txtfile.split('.')[0]
+    cmd = "./src/calculateXSectionAndFilterEfficiency.sh -f {} -y {} -s {} -p {} -n 10000000 -k {}".format(txtfile, year, section, process, skipexisting)
+    print(cmd)
+    os.system(cmd)
+    count+=1
+    print("Processed {} samples.".format(count))
+    
+print("Finished running all the samples.")    
